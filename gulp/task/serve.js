5c85ee40774444460a35c2b754dadf70d2ce1cee
misha <mishashapka@icloud.com>
2019-06-27 14:53:52 +0300

    Настройка gulp

--- gulp/task/serve.js
+++ gulp/task/serve.js
@@ -3,7 +3,7 @@ module.exports = function () {
         return new Promise((res, rej) => {
             $.browsersync.init({
                 server: "./dest/",
-                tunnel: true,
+                //tunnel: true,
                 port: 9000
             });
             res();
