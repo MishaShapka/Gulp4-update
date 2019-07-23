c412c467628bf084a60fd0a56d428ed1438c1c15
misha <mishashapka@icloud.com>
2019-07-23 14:18:23 +0300

    :+1:

--- gulp/task/images.js
+++ gulp/task/images.js
@@ -1,6 +1,6 @@
 module.exports = function() {
     $.gulp.task("images", function() {
-        return $.gulp.src(["./src/img/**/*.{jpg,jpeg,png,gif}", "!./src/img/svg/icons/*", "!./src/img/favicons/*.{jpg,jpeg,png,gif}"])
+        return $.gulp.src(["./src/assets/img/**/*.{jpg,jpeg,png,gif}", "!./src/assets/img/svg/icons/*", "!./src/assets/img/favicons/*.{jpg,jpeg,png,gif}"])
             .pipe($.newer("./dist/img/"))
             .pipe($.imagemin([
                 $.imagemin.gifsicle({interlaced: true}),
--- gulp/task/sass.js
+++ gulp/task/sass.js
@@ -1,25 +1,29 @@
 module.exports = function() {
     $.gulp.task('sass', function(){
-	    return $.gulp.src(['./src/style/app.scss']) // Берем все sass файлы из папки sass и дочерних, если таковые будут
+			// Берем все sass файлы из папки sass и дочерних, если таковые будут
+			return $.gulp.src(['./src/assets/app.scss']) 
 			
-
-			.pipe( $.sass().on( 'error', $.notify.onError( //Уведомление
+			//Уведомление
+			.pipe( $.sass().on( 'error', $.notify.onError( 
 			      {
 			        message: "<%= error.message %>",
 			        title  : "Кэп! Твой код пошел по пизде!"
 			      } )))
 			.pipe($.sass())
 
-
-	        .pipe($.autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true })) //автопрефикс
-			.pipe($.mincss({compatibility: "ie8", level: {1: {specialComments: 0}}})) //Минификация css
-        	//.pipe($.rename({suffix: ".min"})) //Добавление .min
+			 //Автопрефиксы
+			.pipe($.autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
+			//Минификация css
+			.pipe($.mincss({compatibility: "ie8", level: {1: {specialComments: 0}}})) 
+			//Запись всех файлов sass в один css
+			.pipe($.concat('main.css')) 
 			.pipe($.replace("../../dist/", "../"))
-        	.pipe($.plumber.stop())
-        	.pipe($.sourcemaps.write("./maps/"))
-		  //.pipe($.gulp.dest("./dist/css/"))
-			.pipe($.concat('main.css')) //Запись всех файлов sass в один css
-			.pipe($.gulp.dest('./dist/css')) //Каталог в который выгружается конечный css
-        	.on("end", $.browsersync.reload);
+      .pipe($.plumber.stop())
+			.pipe($.sourcemaps.write("./maps/"))		
+			//Добавление .min
+			.pipe($.rename({suffix: ".min"}))
+			//Каталог в который выгружается конечный css
+			.pipe($.gulp.dest('./dist/css')) 
+      .on("end", $.browsersync.reload);
 	});
 };
